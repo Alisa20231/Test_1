4cc93b15761923668207585c5a39a64c587990a7
Alisa20231 <169173697+Alisa20231@users.noreply.github.com>
2024-05-10 18:19:19 +0700

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void drawTriangle(int n) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j <= i; ++j) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "Введите высоту треугольника: ";
+	cin >> n;
+	drawTriangle(n);
+	return 0;
+}
